613b8f17bb8746241cba2ecfa22a35e83830f9da
Thiago <thethiagoriguett@gmail.com>
2026-05-12 15:36:23 -0300

    adicionado client de email

---
+++ src/main/java/com/thiago/bff_agendador_tarefas/infrastructure/client/EmailClient.java
@@ -0,0 +1,19 @@
+package com.thiago.bff_agendador_tarefas.infrastructure.client;
+
+import com.thiago.bff_agendador_tarefas.business.dto.request.EnderecoDTORequest;
+import com.thiago.bff_agendador_tarefas.business.dto.request.LoginRequestDTO;
+import com.thiago.bff_agendador_tarefas.business.dto.request.TelefoneDTORequest;
+import com.thiago.bff_agendador_tarefas.business.dto.request.UsuarioDTORequest;
+import com.thiago.bff_agendador_tarefas.business.dto.response.EnderecoDTOResponse;
+import com.thiago.bff_agendador_tarefas.business.dto.response.TarefasDTOResponse;
+import com.thiago.bff_agendador_tarefas.business.dto.response.TelefoneDTOResponse;
+import com.thiago.bff_agendador_tarefas.business.dto.response.UsuarioDTOResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@FeignClient(name = "notificacao", url = "${notificacao.url}")
+public interface EmailClient {
+
+    void enviarEmail(@RequestBody TarefasDTOResponse dto);
+}
